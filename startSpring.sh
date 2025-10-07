c01b18bffe564061dd7ea067fb1b987ba23eb811
Aman Ojha <amanojhaa@Amans-MacBook-Air.local>
2025-10-07 22:22:57 +0200

    Update start spring shell script to close all processes on 8080 except postman

--- startSpring.sh
+++ startSpring.sh
@@ -4,14 +4,17 @@
 rm -rf /Users/amanojhaa/my-h2-databases/*
 
 # Check for process on TCP port 8080
-PID=$(lsof -ti tcp:8080)
+pids_to_kill=$(lsof -ti tcp:8080 | while read pid; do
+  if ! ps -p $pid -o comm= | grep -iq "Postman"; then
+    echo $pid
+  fi
+done)
 
-if [[ -n "$PID" ]]; then
-  # Kill the process if PID is not empty
-  kill -9 $PID
-  echo "Killed process with PID $PID on port 8080"
+if [ -n "$pids_to_kill" ]; then
+  echo $pids_to_kill | xargs kill -9
+  echo "Killed processes on port 8080 except Postman"
 else
-  echo "No process running on port 8080"
+  echo "No non-Postman processes found on port 8080"
 fi
 
 # Run the Spring Boot application
